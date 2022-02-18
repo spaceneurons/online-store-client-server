21e1c488c1dc565cfdf04fe5de1fdce54a0f6ff8
Margarita <rushevamar@mail.ru>
2022-02-19 02:59:18 +0300

    бд, ройтинг, эндпоинты, обработка ошибок

---
+++ server/controllers/typeController.js
@@ -0,0 +1,15 @@
+const {Type} = require('../models/models')
+const ApiError = require('../error/ApiError')
+class TypeController {
+    async create(req,res){
+const {name}
+    }
+
+    async getAll(req,res) {
+
+    }
+
+}
+
+
+module.exports = new TypeController()
